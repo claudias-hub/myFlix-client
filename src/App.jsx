cf986b3911dfda97be7576829167edf5a55c7e20
claudias-hub <c_oka82@hotmail.com>
2025-05-12 15:16:08 +0200

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { MainView } from './views/main-view/main-view.jsx';
 
 const App = () => {
-  return <MainView />;
+  return <h1>Hello from App</h1>;
 };
 
 export default App;
